a39f4f1985f3680c50a6caf6415ff30941abd6e3
SofiaFrikina <sofi.frikina@yandex.ru>
2023-11-02 18:24:10 +0300

    h

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1/mestodb' } = process.env;
 
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(requestLogger);
 // подключаемся к серверу mongo
 mongoose.connect(DB_URL);
 
@@ -22,7 +20,6 @@ mongoose.connect(DB_URL);
 
 app.use(router);
 app.use(errors());
-app.use(errorLogger);
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
